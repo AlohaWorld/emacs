969e3b26c9c86011be119cf843cb5b312125de40
Cui Yidong <nathan.cui@gmail.com>
2015-12-10 23:49:46 -0600

    由于marmalada包仓库链接有问题，会导致emacs假死，所以暂时去掉

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -96,8 +96,8 @@
 			 '("gnu" . "http://elpa.gnu.org/packages/") t)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+;(add-to-list 'package-archives
+;             '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives 
 			 '("org" . "http://orgmode.org/elpa/") t)
 
